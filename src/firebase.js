2232ad3e8de1f493246b3e24b8c286227735c390
Balram singh <balramsinghraghav1@gmail.com>
2025-11-22 13:07:26 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,9 @@
+// Add your firebase config here
+// export db and auth used in components
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
   apiKey: "AIzaSyALhB6BFnAUEbqI09gW_bW3xr0prK5XeWM",
   authDomain: "boston-real.firebaseapp.com",
@@ -10,6 +12,7 @@ const firebaseConfig = {
   messagingSenderId: "525410685987",
   appId: "1:525410685987:web:5a437c54930667f5de8d96"
 };
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
