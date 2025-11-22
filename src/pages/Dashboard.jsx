e801d292399988e795d9f38282b2f4f451b384bd
Balram singh <balramsinghraghav1@gmail.com>
2025-11-22 15:56:11 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import { collection, onSnapshot, doc, updateDoc } from "firebase/firestore";
 import { db } from "../firebase";
 import { Link } from "react-router-dom";
 import { useAuth } from "../auth.jsx";
-import bg from "../assets/web_bg.png";
+import bg from "../assets/web_bg2.png";
 
 export default function Dashboard() {
   const { user, userDoc } = useAuth();
