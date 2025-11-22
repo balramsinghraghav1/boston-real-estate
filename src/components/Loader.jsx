6342a9085b10def5479365fd282df81d64233ae5
Balram singh <balramsinghraghav1@gmail.com>
2025-11-22 13:53:03 +0530

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,1 +1,16 @@
+import React from "react";
+
+export default function Loader() {
+  return (
+    <div style={{
+      width: "100%",
+      textAlign: "center",
+      padding: "40px",
+      fontSize: "18px",
+      color: "#fff"
+    }}>
+      Loading...
+    </div>
+  );
+}
 
