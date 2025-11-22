0efa1533401d241c2232733e4ee0b3f7242f8379
Balram singh <balramsinghraghav1@gmail.com>
2025-11-22 13:53:53 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,1 +1,14 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../auth.jsx";
+
+export default function ProtectedRoute({ children, role }) {
+  const { user, userDoc } = useAuth();
+
+  if (!user) return <Navigate to="/login" />;
+
+  if (role && userDoc?.role !== role) return <Navigate to="/" />;
+
+  return children;
+}
 
